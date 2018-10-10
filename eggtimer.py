84564bb98781cf5e6a2324f1b568808b00bdeba0
veronica-g <36121577+kethera@users.noreply.github.com>
2018-10-09 21:59:30 -0400

    kettle egg timer

--- eggtimer.py
+++ eggtimer.py
@@ -23,7 +23,7 @@ egg_art = """\
 """
 	clear
 	print egg_art
-	print(The Alton Brown electric kettle egg timer')
+	print('The Alton Brown electric kettle egg timer')
 	input('Boil your egg(s) in the kettle. Hit enter when the kettle shuts itself off')
 	return
 	
@@ -51,27 +51,12 @@ def min_eight():								# One minute
 	time.sleep(60)
 	return
 
-def egg_timer():
-        print('The Alton Brown egg timer!')
-        input('Boil egg(s) in kettle. Hit enter when the kettle shuts itself off')
-
-        first_wait_period()
-        min_seven()
-        min_eight()
-
-        print('Hooray your eggs are cooked, enjoy!')
-        return
 
 #----------------
 # Main program
 #----------------
-<<<<<<< HEAD
 greet()
 first_wait_period()
 min_seven()
 min_eight()
 print('Hooray your eggs are cooked, enjoy!')
-=======
-
-egg_timer()
->>>>>>> 3c955ddc5d5922cffa7823818441bd942cd3725c
