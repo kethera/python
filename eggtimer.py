177e611a557c8c13126fdc51b09d10b3aea292f1
veronica-g <36121577+kethera@users.noreply.github.com>
2018-10-09 22:17:13 -0400

    Update eggtimer.py
    kettle egg timer, inspired by a tweet from alton brown

--- eggtimer.py
+++ eggtimer.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 # last edited oct 9 2018 by veronica
-
 import time
-#----------------
-#Function definitions
-#----------------
+
+#Function definitions:
+#
 def greet():
-	egg_art = '''
-  
+	egg_art = '''  
 '''
 	print (r"""
                        .-~-.
@@ -26,7 +24,8 @@ def greet():
 	print('The Alton Brown electric kettle egg timer\n')
 	input('Boil your egg(s) in the kettle. Press a key when the kettle shuts itself off')
 	return
-	
+#
+#	
 def first_wait_period():
 	x=6											# Alton recommends eight minutes. So we do
 												# six here + 2 1m functions later for msgs
@@ -36,25 +35,23 @@ def first_wait_period():
 		time.sleep(60)							# Sleep one minute per loop iteration
 		
 		x=x-1									# Iterate
-		
 	return										# End of function
 
-
+#
+#
 def min_seven():								# With two minutes left,
 	print('Only two minutes left now!')			# egg the user on
 	time.sleep(60)
 	return
 
-
+#
+#
 def min_eight():								# One minute
 	print('Just sixty seconds to go!')			# warning!
 	time.sleep(60)
 	return
 
-
-#----------------
 # Main program
-#----------------
 greet()
 first_wait_period()
 min_seven()
